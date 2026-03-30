f471981bc076dcbf671e0cc131098743f7eafb90
Mostafa_Taher <mostafa1201@hotmail.com>
2026-03-30 03:40:22 +0200

    [feat]: add validation needed packages + improve prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true
+    })
+  );
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
+  await app.listen(PORT);
 }
 bootstrap();
